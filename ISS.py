9038a7af46d1387d035ce0c617642f25d2ee1d62
Minwitsarut <41381321+Minwitsarut@users.noreply.github.com>
2021-03-26 20:10:31 +0700

    Update ISS.py

--- ISS.py
+++ ISS.py
@@ -4,39 +4,55 @@ import json
 
 
 while True:
-    location = input("Input location : ")
+    location = input('Enter location name : ')  # Bangkok, Washington
     if location[0] == '/':
-        location = location[1::]
-        key = "AdZMk2iGUcusFQipU2yQoqhdJwSXkW3g"
-        main_api = "https://www.mapquestapi.com/geocoding/v1/address?"
-        url = main_api + urllib.parse.urlencode({"key":key, "location":location})
+        location = location[1::].capitalize()
+        # print(location)
+        # latitude & Longitude
+        key = 'AdZMk2iGUcusFQipU2yQoqhdJwSXkW3g'
+        url_geo = 'https://www.mapquestapi.com/geocoding/v1/address?'
+        url = url_geo + urllib.parse.urlencode({'key': key, 'location': location})
         res = requests.get(url)
         result = res.json()
-        lat_lng = result["results"][0]["locations"][0]["latLng"]
+        # print(result)
+        latitude = result['results'][0]['locations'][0]['latLng']['lat']
+        longitude = result['results'][0]['locations'][0]['latLng']['lng']
+        # print(latitude)
+        # print(longitude)
 
-        main_api = "http://api.open-notify.org/iss-pass.json?"
-        url = main_api + urllib.parse.urlencode({"lat":lat_lng["lat"], "lon":lat_lng["lng"]})
+        # ISS  Pass Times
+        url_isspass = 'http://api.open-notify.org/iss-pass.json?'
+        url = url_isspass + urllib.parse.urlencode({'lat': latitude, 'lon': longitude})
         res = requests.get(url)
         result = res.json()
+        # print(result)
+        amount_pass = result['request']['passes']
+        datetime_pass = result['request']['datetime']
+        duration_pass = result['response'][0]['duration']
+        risetime_pass = result['response'][0]['risetime']
+        # print(amount_pass)
+        # print(datetime_pass)
+        # print(duration_pass)
 
-        text1, text2 = '', ''
-        for i in result["response"]:
-            text1 += str(i["risetime"]) + " "
-            text2 += str(i["duration"]) + " "
+        message1 = location + ' is located at Latitude : ' + str(latitude) + ' and Logtitude : ' + str(longitude)
+        # print(message1)
 
-        message1 = "Latitude and Longtitude are " + str(lat_lng["lat"]) + " and " + str(lat_lng["lng"])
-        message2 = "Amount pass : " + str(result["request"]["passes"]) + "\n" + "Rise time are " + text1 + "\n" + "Duration are " + text2
-        print(message1)
-        print(message2)
+        message2 = 'In ' + location + ' the ISS will pass ' + str(amount_pass) + ' times for ' + str(duration_pass) + 's which began to pass ' \
+            + str(datetime_pass) + ' and Rise time are ' + str(risetime_pass)
+        # print(message2)
 
-        # access_token = "-----"
-        # room_id = "Y2lzY29zcGFyazovL3VzL1JPT00vNjA5Nzk5NDAtNTU3My0xMWViLWEzNzUtY2JkMGE4ZjAxYTA3"
-        # url = 'https://webexapis.com/v1/messages'
-        # message = message1 + "\n" + message2
-        # url = 'https://webexapis.com/v1/messages'
-        # headers = {
-        # 'Authorization': 'Bearer {}'.format(access_token),
-        # 'Content-Type': 'application/json'
-        # }
-        # params = {'roomId': room_id, 'markdown': message}
-        # res = requests.post(url, headers=headers, json=params)
+        # Webex
+        access_token = 'OTVhOTA5MzktNzYwMS00ZjNhLWEwODktOGU2MjkxNDVkODhhNjM5YTZjN2YtZTNh_PF84_consumer'
+        room_id = 'Y2lzY29zcGFyazovL3VzL1JPT00vNjA5Nzk5NDAtNTU3My0xMWViLWEzNzUtY2JkMGE4ZjAxYTA3'
+        url = 'https://webexapis.com/v1/messages'
+        message = message1 + '\n' + message2
+        # message = 'Hello' + str(lati_longi['lat'])
+        url = 'https://webexapis.com/v1/messages'
+        headers = {
+            'Authorization': 'Bearer {}'.format(access_token),
+            'Content-Type': 'application/json'
+        }
+        params = {'roomId': room_id, 'markdown': message}
+        res = requests.post(url, headers=headers, json=params)
+
+
